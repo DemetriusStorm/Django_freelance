866513740fe16881df6ce54df960b34b6c36b780
Dmitriy Bulygin <47479409+DemetriusStorm@users.noreply.github.com>
2021-11-08 18:35:46 +0300

    Added swagger (#10)
    * Added swagger
    
    * Added missing file swagger-ui.html
    
    * Replaced the automatic swagger generation with a custom schema.yml
    
    Co-authored-by: Dmitriy Bulygin <mail.bda@yandex.ru>

--- django_freelance/settings.py
+++ django_freelance/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'django_freelance.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -141,6 +141,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
